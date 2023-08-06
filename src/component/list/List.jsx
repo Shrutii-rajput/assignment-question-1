37b8dc32bfbb92ee07ea897995981980935d6c5d
Shruti Rajput <rshruti1112@gmail.com>
2023-08-06 21:35:59 +0530

    new

--- src/component/list/List.jsx
+++ src/component/list/List.jsx
@@ -6,7 +6,9 @@ import ListHeaderCell from "./ListHeaderCell";
 
 import styles from "./List.module.css";
 
-const List = ({ rows }) => {
+const List = ({ rows, selectedItem, orderDetails }) => {
+
+
   return (
     <table className={styles.container}>
       <thead>
@@ -15,17 +17,17 @@ const List = ({ rows }) => {
           <ListHeaderCell>Buy/Sell</ListHeaderCell>
           <ListHeaderCell>Country</ListHeaderCell>
           <ListHeaderCell>Order Submitted</ListHeaderCell>
-          <ListHeaderCell>Order Volume / USD</ListHeaderCell>
+          <ListHeaderCell>{`Order Volume / ${selectedItem}`}</ListHeaderCell>
         </ListHeader>
       </thead>
       <tbody>
         {rows.map((row) => (
-          <ListRow>
-            <ListRowCell>{row["&id"]}</ListRowCell>
+          <ListRow key={row.id} onClick={() => orderDetails(row)}>
+            <ListRowCell>{row.id}</ListRowCell>
             <ListRowCell>{row.executionDetails.buySellIndicator}</ListRowCell>
             <ListRowCell>{row.executionDetails.orderStatus}</ListRowCell>
-            <ListRowCell>{row.orderSubmitted}</ListRowCell>
-            <ListRowCell>{row.bestExecutionData.orderVolume.USD}</ListRowCell>
+            <ListRowCell>{row.submittedDate}</ListRowCell>
+            <ListRowCell>{row.bestExecutionData.orderVolume[selectedItem]}</ListRowCell>
           </ListRow>
         ))}
       </tbody>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -17,13 +17,54 @@ import Card from "../component/card/Card";
 const Dashboard = () => {
   const [currency, setCurrency] = useState("EUR");
   const [searchText, setSearchText] = useState("");
-  const [selectedOrderDetails, setSelectedOrderDetails] = useState({});
-  const [selectedOrderTimeStamps, setSelectedOrderTimeStamps] = useState({});
+  const [selectedOrderDetails, setSelectedOrderDetails] = useState({
+    buySellIndicator: '',
+    orderStatus: '',
+    orderType: '',
+  });
+  const [selectedOrderTimeStamps, setSelectedOrderTimeStamps] = useState({
+    orderReceived: '',
+    orderStatusUpdated: '',
+    orderSubmitted: '',
+  });
+
+  const combinedData = mockData.results.map(order => {
+    const { "&id":id, ...rest } = order;
+    const timestampsData = timestamps.results.find(item => item["&id"] === id);
+    const submittedDate = timestampsData.timestamps.orderSubmitted;
+    const recievedDate = timestampsData.timestamps.orderRecieved;
+    const updatedDate = timestampsData.timestamps.orderStatusUpdated;
+    return {
+      ...rest,
+      id,
+      submittedDate,
+      recievedDate,
+      updatedDate,
+    };
+  });
+
+  const filteredData = combinedData.filter(order => order.id.toLowerCase().includes(searchText.toLowerCase()));
+
+  const handleOrderDetails = (row) => {
+    const orderDetails = {
+      buySellIndicator: row.executionDetails.buySellIndicator,
+      orderStatus: row.executionDetails.orderStatus,
+      orderType: row.executionDetails.orderType,
+    };
+    setSelectedOrderDetails(orderDetails);
+
+    const orderTimeStamps = {
+      orderReceived: row.timestamps.orderReceived,
+      orderStatusUpdated: row.timestamps.orderStatusUpdated,
+      orderSubmitted: row.timestamps.orderSubmitted,
+    };
+    setSelectedOrderTimeStamps(orderTimeStamps);
+  }
 
   return (
     <div>
       <div className={styles.header}>
-        <HeaderTitle primaryTitle="Orders" secondaryTitle="5 orders" />
+        <HeaderTitle primaryTitle="Orders" secondaryTitle={`${mockData.results.length} Orders`} />
         <div className={styles.actionBox}>
           <Search
             value={searchText}
@@ -47,7 +88,7 @@ const Dashboard = () => {
             title="Selected Order Timestamps"
           />
         </div>
-        <List rows={mockData.results} />
+        <List rows={filteredData} selectedItem={currency} orderDetails={handleOrderDetails} />
       </div>
     </div>
   );
